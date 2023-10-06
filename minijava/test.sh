be3d0bd8e133d205898e3b3ef21f7c64c2ff3fc3
Surugiu Dragos <dragos.surugiu13@yahoo.com>
2023-10-06 18:38:05 +0300

    Added Google Tests dependencies and an initial test

---
+++ minijava/test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+current_dir=$PWD;
+
+cd build/;
+
+MACHINE_TYPE=$(uname -m)
+echo "Found the folowing arch: <$MACHINE_TYPE>"
+
+if [ "${MACHINE_TYPE}" == "arm64" ]
+then
+    cmake ../ -G Ninja DBUILD_SHARED_LIBS:BOOL=OFF;
+elif [ "${MACHINE_TYPE}" == "x86_64" ]
+then
+    cmake ../ -G Ninja  -DBUILD_SHARED_LIBS:BOOL=OFF;
+fi
+
+CORES=""
+if [ "${MACHINE_TYPE}" == "arm64" ]
+then
+    CORES=$(sysctl -n hw.ncpu)
+elif [ "${MACHINE_TYPE}" == "x86_64" ]
+then
+    CORES=$(nproc --all)
+fi
+echo "Machine has <$CORES> cores."
+
+if cmake --build . -j$CORES; then 
+echo "Build succeded!";
+else 
+echo "Build failed, operation will be aborted!";
+exit
+fi
+
+test/MiniJavaCompiler-test
---
+++ minijava/test/MiniJavaParsingTest.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+
+
+class MiniJavaParsingTest : public ::testing::Test
+{
+    void setUp()
+    {
+    }
+
+    void tearDown()
+    {
+    }
+
+public:
+
+};
+
+TEST_F(MiniJavaParsingTest, AssertSomeValue)
+{
+    ASSERT_TRUE(true);
+}
+
